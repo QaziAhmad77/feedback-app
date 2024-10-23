d2ecaa918ad5f215f4e8fbcfca7a89fc1456bbf8
QaziAhmad77 <qaziahmad1877@gmail.com>
2024-10-23 17:50:09 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    }
-}
-// const nextConfig = {
-//     experimental: {
-//         appDir: false,
-//     }
-// }
+const nextConfig = {}
 
 module.exports = nextConfig
-
